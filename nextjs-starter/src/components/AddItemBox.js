377fcf1195d8be2d1a5a9984531ef24d63244a36
Ryan <exner014@umn.edu>
2023-04-11 19:04:16 -0500

    add task form

--- nextjs-starter/src/components/AddItemBox.js
+++ nextjs-starter/src/components/AddItemBox.js
@@ -5,14 +5,41 @@ function AddItemBox({show, handleShowBox}) {
 
     if (show) {
         return (
-            <div>
-            <form>
-                <label>
-                    Name:
-                    <input type="text" name="name" />
-                </label>
+            <div style={{paddingTop: "1em"}}>
+            <form id="usrform">
+            <div className="field">
+                <label className="label">Task Name</label>
+                <div className="control">
+                    <input className="input" type="text" placeholder="Text input" required></input>
+                </div>
+            </div>
+            <div className="field">
+            <label className="label">Category</label>
+            <div className="control">
+                <div className="select">
+                <select  name="category" id="category" required>
+                    <option value="">Select Category</option>
+                    <option value="1">Option1</option>
+                </select>
+                </div>
+            </div>
+            </div>
+            <div className="field">
+            <label className="label">Description</label>
+            <div className="control">
+                <textarea className="textarea" placeholder="Textarea" required></textarea>
+            </div>
+            </div>
+            <div className="field is-grouped">
+            <div className="control">
+                <button className="button is-link">Submit</button>
+            </div>
+            <div className="control">
+                <button onClick={handleShowBox} className="button is-link is-light">Cancel</button>
+            </div>
+            </div>
             </form>
-            <button onClick={handleShowBox}>click to cancel</button>
+            
             </div>
         )
     }
